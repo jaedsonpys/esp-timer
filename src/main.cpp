b77b51cfb343963edc000c3eb1745bb333d8a224
Jaedson Silva <jaedson.dev@proton.me>
2023-06-23 17:53:41 -0300

    feat: connect esp32 to wifi network

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+
+#define RPIN 15
+
+const char* ssid = "JARMESON_JNETCOM";
+const char* password = "wet20110";
+
+WiFiUDP ntpUDP;
+NTPClient ntp(ntpUDP);
+
+void setup() {
+    pinMode(RPIN, OUTPUT);
+    pinMode(2, OUTPUT);
+
+    WiFi.begin(ssid, password);
+    while(WiFi.status() != WL_CONNECTED) {
+        digitalWrite(2, HIGH);
+        delay(200);
+        digitalWrite(2, LOW);
+        delay(200);
+    }
+}
+
+void loop() {
+
+}
