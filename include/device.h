e1882d5ae20e0348a712d34daaf98f46aeec1cb0
Jaedson Silva <jaedson.dev@proton.me>
2023-06-30 12:36:21 -0300

    feat: create device file header

---
+++ include/device.h
@@ -0,0 +1,26 @@
+#ifndef DEVICE_h
+#define DEVICE_h
+#endif
+
+#include <Arduino.h>
+
+class Device {
+    private:
+        bool timerEnabled, timerIsRunning;
+        int secondsOnAfterStart, lastDayTimer, devicePin;
+        int timerStartHour, timerStartMinute, timerEndHour, timerEndMinute;
+
+        String deviceName;
+
+    public:
+        void begin(String deviceName, int devicePin);
+
+        void setTimer(int startHour, int startMinute, int endHour, int endMinute);
+        void deleteTimer();
+
+        void powerOn();
+        void powerOff();
+
+        String getTimerStart();
+        String getTimerEnd();
+};
