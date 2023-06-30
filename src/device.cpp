5e490ae1f0c1514a89c97e40d878d6e8788a37eb
Jaedson Silva <jaedson.dev@proton.me>
2023-06-30 13:12:48 -0300

    feat: create setTimer() function

--- src/device.cpp
+++ src/device.cpp
@@ -7,3 +7,36 @@ Device::Device(String deviceName, int devicePin) {
 
     pinMode(devicePin, OUTPUT);
 }
+
+void Device::setTimer(int startHour, int startMinute, int endHour, int endMinute) {
+    this->timerStartHour = startHour;
+    this->timerStartMinute = startMinute;
+    this->timerEndHour = endHour;
+    this->timerEndMinute = endMinute;
+
+    int timeDiff, minutesDiff;
+
+    if(startHour > endHour) {
+        int startHourSub = 24 - startHour;
+        timeDiff = endHour + startHourSub;
+    } else {
+        timeDiff = endHour - startHour;
+    }
+
+    minutesDiff = endMinute - startMinute;
+
+    if(minutesDiff < 0) {
+        minutesDiff *= -1;
+    }
+
+    this->secondsOnAfterStart = (timeDiff * 3600) + (minutesDiff * 60);
+
+    xTaskCreate(
+        timerTask,
+        "timerTask",
+        4000,
+        NULL,
+        1,
+        &this->TimerTaskHandle
+    );
+}
