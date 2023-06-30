b5ccb421f7789c0df8d6d5838cc9c821d4ccf2ee
Jaedson Silva <jaedson.dev@proton.me>
2023-06-30 17:22:05 -0300

    feat: define default values to attributes

--- include/device.h
+++ include/device.h
@@ -7,8 +7,12 @@
 class Device {
     private:
         bool timerIsRunning, timerIsCreated = false;
-        int secondsOnAfterStart, lastDayTimer, devicePin;
-        int timerStartHour, timerStartMinute, timerEndHour, timerEndMinute;
+        int secondsOnAfterStart = 0, lastDayTimer = 0, devicePin = 0;
+
+        int timerStartHour = 22;
+        int timerStartMinute = 00;
+        int timerEndHour = 06;
+        int timerEndMinute = 00;
 
         String deviceName;
         TaskHandle_t TimerTaskHandle;
