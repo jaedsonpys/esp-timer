78fd417354a8a3e03c8b42d066eed2ec80e35f87
Jaedson Silva <jaedson.dev@proton.me>
2023-09-24 11:12:37 -0300

    feat: remove WebServer routes

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <WebServer.h>
 
 #include "device.h"
 
@@ -18,8 +17,6 @@ IPAddress gateway(192, 168, 0, 1);
 IPAddress subnet(255, 255, 255, 0);
 IPAddress dns1(8, 8, 8, 8);
 IPAddress dns2(8, 8, 4, 4);
-
-WebServer server(80);
 
 Device device01("LampadaSala", 15);
 
@@ -44,84 +41,8 @@ void setup() {
     }
 
     configTime(gmtOffsetSec, daylightOffSetSec, mainNTPServer, recoveryNTPServer);
-
-    server.on("/timer", HTTP_GET, getTimer);
-    server.on("/timer", HTTP_POST, setTimer);
-    server.on("/timer", HTTP_OPTIONS, sendCORSHeader);
-    server.on("/device", HTTP_GET, getDeviceStatus);
-    server.on("/device", HTTP_POST, controlDevice);
-    server.on("/device", HTTP_OPTIONS, sendCORSHeader);
-
-    server.begin();
 }
 
 void loop() {
-    server.handleClient();
-}
 
-void setTimer() {
-    server.sendHeader(F("Access-Control-Allow-Origin"), F("*"));
-
-    String sH = server.arg("sh");
-    String sM = server.arg("sm");
-    String eH = server.arg("eh");
-    String eM = server.arg("em");
-    String status = server.arg("status");
-
-    if(status == "off") {
-        device01.deleteTimer();
-        server.send(200, F("text/pain"), F("timer deleted"));
-    } else if(sH != "" && sM != "" && eH != "" && eM != "") {
-        device01.setTimer(sH.toInt(), sM.toInt(), eH.toInt(), eM.toInt());
-        server.send(201, F("text/pain"), F("timer created"));
-    } else {
-        server.send(400, F("text/pain"), F("invalid data"));
-    }
-}
-
-void sendCORSHeader() {
-    server.sendHeader(F("Access-Control-Allow-Origin"), F("*"));
-    server.sendHeader(F("Access-Control-Max-Age"), F("600"));
-    server.sendHeader(F("Access-Control-Allow-Methods"), F("PUT,POST,GET,OPTIONS"));
-    server.sendHeader(F("Access-Control-Allow-Headers"), F("*"));
-    server.send(204);
-}
-
-void getTimer() {
-    server.sendHeader(F("Access-Control-Allow-Origin"), F("*"));
-
-    String start = device01.getTimerStart();
-    String end = device01.getTimerEnd();
-    String status = device01.timerIsActive() ? "true" : "false";
-
-    String startKV = "\"start\": \"" + start + "\",";
-    String endKV = "\"end\": \"" + end + "\",";
-    String statusKV = "\"timerActive\": " + status;
-    String response = "{" + startKV + endKV + statusKV + "}"; 
-
-    server.send(200, "application/json", response);
-}
-
-void controlDevice() {
-    server.sendHeader(F("Access-Control-Allow-Origin"), F("*"));
-    String status = server.arg("status");
-
-    if(status == "on"){
-        device01.powerOn();
-    } else if(status == "off") {
-        device01.powerOff();
-    }
-
-    if(status == "on" || status == "off") {
-        server.send(200);
-    } else {
-        server.send(400);
-    }
-}
-
-void getDeviceStatus() {
-    server.sendHeader(F("Access-Control-Allow-Origin"), F("*"));
-    String status = device01.isON() ? "on" : "off";
-    String response = "{\"status\":\"" + status + "\"}";
-    server.send(200, "application/json", response);
 }
